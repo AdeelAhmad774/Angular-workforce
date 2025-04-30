e566410222fffa4b1fe6f0d0264937ef8d354d0a
Adeel Ahmed <bepolite744@gmail.com>
2025-04-30 20:39:16 +0500

    Your commit message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    LoginComponent
+    LoginComponent,
+    MainDashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+MatMenuModule,
+MatFormFieldModule,
+   
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
